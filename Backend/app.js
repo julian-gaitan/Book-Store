a3763352c97dbfeddd8e23de4951cb14aa6fea5e
Julian Gaitan <julian89net@gmail.com>
2024-02-22 23:10:15 -0500

    Add initial setup for DB connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, USER, PASSWORD } from "./.config.js";
 
 const app = express();
+const DBurl = `mongodb+srv://${USER}:${PASSWORD}@cluster0.zmamtu8.mongodb.net/books-collection?`;
 
-app.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT}`);
+app.get('/', (req, res) => {
+    console.log(req.headers);
+    res.status(200).send('Hello!');
 });
+
+mongoose.connect(DBurl)
+    .then(() => {
+        console.log('App connected to DB');
+        app.listen(PORT, () => {
+            console.log(`App listening on port: ${PORT}`);
+        });        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
