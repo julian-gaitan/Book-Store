08610d36ac81d812d84f18ba7b20bcc2b55ecbb5
Julian Gaitan <julian89net@gmail.com>
2024-02-23 21:46:58 -0500

    Add API endpoint '/books' and 'books/:id' to Read books in the DB

--- Backend/app.js
+++ Backend/app.js
@@ -12,28 +12,53 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(req.headers);
-    res.status(200).send('Hello!');
+    res.status(200).send('Bookstore API!');
 });
 
-app.post('/books', async (req, res) => {
+async function testForError(func, res) {
     try {
-        const { title, author, publishYear } = req.body;
-        if (!title || !author || !publishYear) {
-            return res.status(400).send({ error: 'All the fields are require: title, author, publishYear' });
-        }
-        const newBook = {
-            title,
-            author,
-            publishYear,
-        };
-        console.log(newBook);
-        const book = await Book.create(newBook);
-        res.status(201).send(book);
+        await func();
     } catch (err) {
         console.log(err.message);
         res.status(500).send({ error: err.message });
     }
-});
+}
+
+app.route('/books')
+    .get(async (req, res) => {
+        testForError(async () => {
+            const books = await Book.find({});
+            res.status(200).send({
+                count: books.length,
+                books,
+            });
+        }, res);
+    })
+    .post(async (req, res) => {
+        testForError(async () => {
+            const { title, author, publishYear } = req.body;
+            if (!title || !author || !publishYear) {
+                return res.status(400).send({ error: 'All the fields are require: title, author, publishYear' });
+            }
+            const newBook = {
+                title,
+                author,
+                publishYear,
+            };
+            console.log(newBook);
+            const book = await Book.create(newBook);
+            res.status(201).send(book);
+        }, res);
+    });
+
+app.route('/books/:id')
+    .get(async (req, res) => {
+        testForError(async () => {
+            const { id } = req.params;
+            const book = await Book.findById(id) || {};
+            res.status(200).send(book);
+        }, res)
+    });
 
 mongoose.connect(DBurl)
     .then(() => {
