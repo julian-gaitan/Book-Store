6e9366e712a3c6a03989bdfa9745fd3306b84237
Julian Gaitan <julian89net@gmail.com>
2024-03-05 20:16:05 -0500

    Implement BookCards.jsx to display all books in the form of cards

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -28,28 +28,41 @@ const Home = () => {
 
   return (
     <div className="py-4 px-8">
+      <div className="flex justify-center items-center gap-x-4">
+        <Button onClick={() => setMode('Table')} active={mode === 'Table'}>
+          Table
+        </Button>
+        <Button onClick={() => setMode('Cards')} active={mode === 'Cards'}>
+          Cards
+        </Button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8 ms-4">Books List</h1>
         <Link to="/books/create" className="me-4">
           <MdOutlineAddBox className="text-5xl text-sky-700" />
         </Link>
       </div>
-      <button onClick={() => setMode('Table')}>Table</button>
-      <button onClick={() => setMode('Cards')}>Cards</button>
       {books.length ? (
-        (mode === 'Table') ? (
+        mode === 'Table' ? (
           <BookTable books={books} />
-        ) : (
-          (mode === 'Cards') ? (
-            <BookCards books={books} />
-          ) : (
-            null
-          )
-        )
+        ) : mode === 'Cards' ? (
+          <BookCards books={books} />
+        ) : null
       ) : (
         <Spinner />
       )}
     </div>
+  );
+};
+
+const Button = ({ children, onClick, active }) => {
+  return (
+    <button
+      className={'bg-sky-200 hover:bg-sky-500 px-4 py-1 rounded-lg' + (active ? ' border-2 border-sky-700' : '')}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 };
 
